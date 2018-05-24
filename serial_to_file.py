0ec362e3935474e5fa451ef92ee69935f7cd0a36
Owen Osborn <owen@critterandguitari.com>
2018-05-24 14:17:14 -0400

    adding arduino sketch and python logger

---
+++ serial_to_file.py
@@ -0,0 +1,23 @@
+##############
+## Script listens to serial port and writes contents into a file
+##############
+## requires pySerial to be installed 
+import serial
+import time
+
+serial_port = '/dev/cu.usbmodem1431';
+baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
+write_to_file_path = "output.txt";
+
+output_file = open(write_to_file_path, "w+");
+ser = serial.Serial(serial_port, baud_rate)
+
+start_time = time.time()
+
+while True:
+    line = ser.readline();
+    line = line.decode("utf-8") #ser.readline returns a binary, convert to string
+    line = str(time.time() - start_time) + ", " + line
+    print(line);
+    output_file.write(line);
+    output_file.flush();
